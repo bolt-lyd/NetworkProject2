06333e77737d0c53564fe99aa43de1d2069d4200
kelly deal <kellyanndeal@gmail.com>
2024-04-10 21:29:50 -0400

    fix bugs in part 1

--- client.py
+++ client.py
@@ -1,29 +1,36 @@
 import socket
 import threading
 
+
 # send messages
 def send_message(client_socket):
     while True:
         message = input()
         client_socket.sendall(message.encode())
-            
+
 
 def receive_message(client_socket):
     while True:
         try:
             data = client_socket.recv(1024).decode()
             if data:
-                print(data)
+                print(data + '\n')
         except Exception as e:
             print(f"Error: ", e)
             break
 
 #start client
 def start_client():
+    server_address = input("Enter server address: ")
+    server_port = int(input("Enter server port: "))
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((server_address, server_port))
 
     send = threading.Thread(target=send_message, args=(client_socket,))
     receive = threading.Thread(target=receive_message, args=(client_socket,))
 
     send.start()
     receive.start()
+
+# Start the client
+start_client()
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
-from pydoc import cli
+import datetime
 import socket
 import threading
-import datetime
+from pydoc import cli
 
 ################## Initializations ##################
 
@@ -10,28 +10,7 @@ HOST = '127.0.0.1'
 PORT = 12345
 
 #Help Menu
-help_menu = f"Commands:
-- JOIN:
-    Join the message board and assign your username.
-    Example: JOIN;username
-- POST:
-    Post to the message board.
-    Example: POST;Post Subject;Post body.
-- USERS:
-    Returns list of users in the message board.
-    Example: USERS
-- MESSAGE:
-    Gets the Post body of the requested post.
-    Example: MESSAGE;Id
-- LEAVE:
-    Leaves the given group.
-    Example: LEAVE
-- EXIT:
-    Exit the server.
-    Example: EXIT
-- HELP:
-    View the this menu again at any time.
-    Example: HELP"
+help_menu = "Commands:\n- JOIN:\nJoin the message board and assign your username.\nExample: JOIN;username\n- POST:\nPost to the message board.\nExample: POST;Post Subject;Post body.\n- USERS:\nReturns list of users in the message board.\nExample: USERS\n- MESSAGE:\nGets the Post body of the requested post.\nExample: MESSAGE;Id\n- LEAVE:\nLeaves the given group.\nExample: LEAVE\n- EXIT:\nExit the server.\nExample: EXIT\n- HELP:\nView the this menu again at any time.\nExample: HELP"
 
 # Dictionary for tacking client information
 client_info = {}
@@ -47,6 +26,7 @@ client_info = {
 '''
 posts = {}
 post_id = 0
+username = None
 ################## /Initializations ##################
 
 
@@ -54,55 +34,85 @@ post_id = 0
 
 # Handle incoming client connections
 def handle_client(client_socket, client_address):
-    
+    global post_id
+    global username
     while True:
         try:
             data = client_socket.recv(1024).decode()
-            if data.startswith('CONNECT'):
-                new_connection = data.split(';')
-                client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                client_socket.connect((new_connection[1], new_connection[2]))
-            elif data.startswith('JOIN'):
+            # if data.startswith('CONNECT'):
+            #     new_connection = data.split(';')
+            #     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            #     client_socket.connect((new_connection[1], new_connection[2]))
+
+            #tested;works
+            if data.startswith('JOIN'):
                 username = data.split(';')[1]
                 if username in client_info.keys():
                     client_socket.send("Username already exists. Please join with a new username.".encode("utf-8"))
-                    client_socket.close()
                 else:
                     client_info[username] = client_socket
-                    client_socket.send(help_menu.encode)
-                message = str(username) + " has joined the server"
-                broadcast(message.encode())
+                    #client_socket.send(help_menu.encode())
+                    if len(posts) > 1:
+                        post = posts[post_id - 1]
+                        client_socket.send(f"{post_id - 1}, {post['username']}, {post['date']}, {post['subject']}".encode())
+                    if len(posts) > 0:
+                        post = posts[post_id]
+                        client_socket.send(f"{post_id}, {post['username']}, {post['date']}, {post['subject']}".encode())
+                    message = f"{username} has joined the server"
+                    broadcast(message)
+
+            #tested; works
             elif data.startswith('POST'):
-                post_id += 1
-                new_post = data.split(';')
-                post = f"{post_id}, {username}, {datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")}, {new_post[1]}"
-                posts[post_id] = {'username:', username, 
-                                'date:', {datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")}, 
-                                'subject:', {new_post[1]},
-                                'body:', {new_post[2]}}
-                broadcast(post)
+                if not username:
+                    client_socket.send("Please join the server first.".encode("utf-8"))
+                else:
+                    post_id += 1
+                    new_post = data.split(';')
+                    post = f"{post_id}, {username}, {datetime.datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, {new_post[1]}"
+                    posts[post_id] = {'username': username,
+                                    'date': datetime.datetime.now().strftime('%m/%d/%Y %H:%M:%S'),
+                                    'subject': new_post[1],
+                                    'body': new_post[2]}
+                    broadcast(post)
+
+            # tested; works
+            # displays list of users currently in server group to the client
             elif data.startswith('USERS'):
-                message = "Users in group: " + {list(client_info.keys())}
-                client_socket.send(message.encode("utf-8"))
+                if not username:
+                    client_socket.send("Please join the server first.".encode("utf-8"))
+                else:
+                    message = "Users in group: " + ", ".join(client_info.keys())
+                    client_socket.send(message.encode("utf-8"))
+
             elif data.startswith('LEAVE'):
-                user = client_info[username]
-                message = str(username) + " has left the server"              
-                broadcast(message.encode())
+                message = str(username) + " has left the server"
+                broadcast(message)
                 del client_info[username]
+                break
+
             elif data.startswith('MESSAGE'):
+                message = data.split(';')
                 try:
-                    message = data.split(';')
-                    m = posts[message[1]]
-                    post = f"User: , {m[0]['username']}, \nDate: , {m[0]['date']}, \nSubject: , {m[0]['subject']}, \nBody: {m[0]['body']}"
-                    client_socket.send(post)
-                except Exception as e:
-                    client_socket.send("Message ID not found.")
+                    post_info = posts[int(message[1])]
+                    post_message = f"Post ID: {message[1]}\nUsername: {post_info['username']}\nDate: {post_info['date']}\nSubject: {post_info['subject']}\nBody: {post_info['body']}"
+                    client_socket.send(post_message.encode())
+                except:
+                    client_socket.send("Post not found.".encode())
+
+            #tested; works
+            # terminates client
             elif data.startswith('EXIT'):
                 client_socket.close()
+                print(f"{client_address} disconnected")
+                break
+
+            #tested; works
+            # prints help menu to the client
             elif data.startswith('HELP'):
                 client_socket.send(help_menu.encode())
+
             else:
-                client_socket.send('Not valid command'.encode("utf-8"))                
+                client_socket.send('Not valid command'.encode("utf-8"))
 
         except Exception as e:
             print("Error:", e)
@@ -110,13 +120,15 @@ def handle_client(client_socket, client_address):
 
 # Send message to everyone
 def broadcast(message):
-    for username in client_info.items():
-        client_socket = client_info[username]
+    for client_socket in client_info.values():
         try:
-            client_socket.send(message.encode())
+            client_socket.sendall(str(message).encode())
         except Exception as e:
             print("Error:", e)
-        
+
+def get_post(id):
+    return
+
 ################## /Functions ##################
 
 
@@ -124,6 +136,8 @@ def broadcast(message):
 
 # Create a socket for server
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HOST = input("Input server's host: ")
+PORT = int(input("Input server's port: "))
 server_socket.bind((HOST, PORT))
 server_socket.listen()
 print(f"Server listening on {HOST}:{PORT}")
@@ -137,9 +151,10 @@ try:
     while True:
         client_socket, client_address = server_socket.accept()
         print(f"Client connected from {client_address}")
+        client_socket.send(help_menu.encode())
 
         # New thread for handling the client
-        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_thread.start()))
+        threading.Thread(target=handle_client, args=(client_socket, client_address)).start()
 
 except KeyboardInterrupt:
     print("Server shutting down...")
